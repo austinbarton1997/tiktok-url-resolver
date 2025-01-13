260de26473eb103ae1723f8902d97e0fbb20f868
John Doe <austin.barton1997@icloud.com>
2025-01-12 22:23:13 -0600

    test

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const backendUrl = 'https://tiktok-url-resolver.onrender.com'; // Replace with your Render backend URL
+const backendUrl = 'https://www.austinbarton.dev'; // Replace with your Render backend URL
 
 document.getElementById('resolveBtn').addEventListener('click', async () => {
   const shortUrl = document.getElementById('shortUrl').value.trim();
